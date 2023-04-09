a9224bbe074f2b3d8f67135fc59d071cd4cef0b7
Dmitry Samsonov <dmitry.samsonov@ecentria.com>
2023-04-09 18:03:32 -0500

    Publishing messages to RabbitMQ

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using RabbitMQ.Client;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Start");
+
+const string exchangeName = "amq.direct";
+const string queueName = "some-queue";
+const string routingKey = "some-routing-key";
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queueName, true, false, false);
+channel.QueueBind(queueName, exchangeName, routingKey);
+
+while (true)
+{
+    Console.WriteLine("Enter message payload (print \"exit\" to finish):");
+    var payload = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(payload))
+        continue;
+    
+    if (payload == "exit")
+        break;
+
+    var body = Encoding.UTF8.GetBytes(payload);
+
+    channel.BasicPublish(exchangeName, routingKey, null, body);
+
+    Console.WriteLine("Message sent");
+}
+
+Console.WriteLine("Finish");
