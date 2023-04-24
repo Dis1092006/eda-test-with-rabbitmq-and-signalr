42baadc94a1edcd0eba1d6cc01f123a3c98bcb80
Dmitry Samsonov <dmitry.samsonov@ecentria.com>
2023-04-23 19:06:30 -0500

    Add SignalR client

---
+++ SignalRClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+Console.WriteLine("SignalR client starting ...");
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("https://localhost:7180/hub")
+    .Build();
+await connection.StartAsync();
+
+connection.On<string, string>("ReceiveMessage", (user, message) =>
+{
+    Console.WriteLine($"{user}: {message}");
+});
+
+Console.Read();
