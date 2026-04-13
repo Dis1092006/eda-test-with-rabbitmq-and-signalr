86142862b63c4b0c4032dcdc935eae28db47c58e
Dmitry Samsonov <Dis1092006@gmail.com>
2026-04-13 09:52:08 -0500

    feat: add Subscribe method and cleanup on disconnect to MessageHub

--- SignalRHub/MessageHub.cs
+++ SignalRHub/MessageHub.cs
@@ -1,11 +1,31 @@
 using Microsoft.AspNetCore.SignalR;
+using SignalRHub.Services;
 
 namespace SignalRHub;
 
 public class MessageHub : Hub
 {
+    private readonly FilterService _filterService;
+
+    public MessageHub(FilterService filterService)
+    {
+        _filterService = filterService;
+    }
+
     public async Task SendMessage(string user, string message)
     {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+
+    public Task Subscribe(string filter)
+    {
+        _filterService.SetFilter(Context.ConnectionId, filter);
+        return Task.CompletedTask;
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        _filterService.RemoveFilter(Context.ConnectionId);
+        return base.OnDisconnectedAsync(exception);
     }
 }
