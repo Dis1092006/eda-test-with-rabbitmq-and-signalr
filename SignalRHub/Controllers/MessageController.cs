91155237a77dc441dab6a77dc3f96e94eb1c446e
Dmitry Samsonov <Dis1092006@gmail.com>
2026-04-13 09:57:59 -0500

    feat: broadcast ReceiveFilteredMessage to subscribed connections

--- SignalRHub/Controllers/MessageController.cs
+++ SignalRHub/Controllers/MessageController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using SignalRHub.Services;
 
 namespace SignalRHub.Controllers;
 
@@ -8,17 +9,27 @@ namespace SignalRHub.Controllers;
 public class MessageController : ControllerBase
 {
     private readonly IHubContext<MessageHub> _hubContext;
+    private readonly FilterService _filterService;
 
-    public MessageController(IHubContext<MessageHub> hubContext)
+    public MessageController(IHubContext<MessageHub> hubContext, FilterService filterService)
     {
         _hubContext = hubContext;
+        _filterService = filterService;
     }
-    
+
     [HttpPost]
     [Route("/message")]
     public async Task<IActionResult> Post([FromBody] string message)
     {
         await _hubContext.Clients.All.SendAsync("ReceiveMessage", "Server", message);
+
+        var matchingConnections = _filterService.GetMatchingConnections(message).ToList();
+        if (matchingConnections.Count > 0)
+        {
+            await _hubContext.Clients.Clients(matchingConnections)
+                .SendAsync("ReceiveFilteredMessage", "Server", message);
+        }
+
         return Ok();
     }
 }
