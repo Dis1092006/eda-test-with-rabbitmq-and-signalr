1bf6459a5dfffc7805abda088a87f08442e8f7ad
Dmitry Samsonov <Dis1092006@gmail.com>
2026-04-13 09:58:03 -0500

    feat: register FilterService singleton and add CORS policy for Vue dev server

--- SignalRHub/Program.cs
+++ SignalRHub/Program.cs
@@ -1,11 +1,20 @@
 using Scalar.AspNetCore;
 using SignalRHub;
+using SignalRHub.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSignalR();
+builder.Services.AddSingleton<FilterService>();
+
+builder.Services.AddCors(options =>
+    options.AddPolicy("VueDev", policy =>
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials()));
 
 var app = builder.Build();
 
@@ -16,6 +25,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("VueDev");
 app.UseAuthorization();
 
 app.MapControllers();
