7ba1048b72928536d911d74ff439f52f6ec77e58
Dmitry Samsonov <Dis1092006@gmail.com>
2026-04-13 10:54:05 -0500

    feat: wire up App.vue with Vuetify layout and SignalR startup

--- VueSignalRClient/src/App.vue
+++ VueSignalRClient/src/App.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <v-app>
+    <v-app-bar color="primary" flat>
+      <v-app-bar-title>SignalR Live Dashboard</v-app-bar-title>
+    </v-app-bar>
+    <v-main style="height: 100vh">
+      <v-container fluid class="pa-4" style="height: calc(100% - 64px)">
+        <v-row style="height: 100%">
+          <v-col cols="6" style="height: 100%">
+            <AllMessages />
+          </v-col>
+          <v-col cols="6" style="height: 100%">
+            <FilteredMessages />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { startConnection } from '@/services/signalr'
+import AllMessages from '@/components/AllMessages.vue'
+import FilteredMessages from '@/components/FilteredMessages.vue'
+
+onMounted(() => {
+  startConnection()
+})
+</script>
--- VueSignalRClient/src/main.ts
+++ VueSignalRClient/src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({ components, directives, theme: { defaultTheme: 'light' } })
+
+createApp(App)
+  .use(createPinia())
+  .use(vuetify)
+  .mount('#app')
